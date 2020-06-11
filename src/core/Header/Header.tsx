de4154d9b061f4d8eb097187e52c21588ffb380a
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-11 17:12:38 -0300

    Add Header

---
+++ src/core/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+import styles from './Header.module.scss';
+
+const Header: FC = () => (
+  <header className={styles.header}>
+  </header>
+);
+
+export default Header;
