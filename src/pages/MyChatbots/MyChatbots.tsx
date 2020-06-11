d8906c4b0b15e6cdd9da656cdeec1caa51e3e207
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-11 17:32:10 -0300

    Add MyChatbots

---
+++ src/pages/MyChatbots/MyChatbots.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import styles from './MyChatbots.module.scss';
+
+const MyChatbots: FC = () => (
+  <section className={styles.myChatbots}></section>
+);
+
+export default MyChatbots;
