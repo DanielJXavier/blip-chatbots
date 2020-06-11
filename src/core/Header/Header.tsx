e7e5e27c2c810475bae529b2192ba8551cdb5330
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-11 17:12:44 -0300

    Add logo

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 
+import logo from 'assets/images/logo.png';
+
 import styles from './Header.module.scss';
 
 const Header: FC = () => (
   <header className={styles.header}>
+    <img src={logo} alt="BLiP" />
   </header>
 );
 
