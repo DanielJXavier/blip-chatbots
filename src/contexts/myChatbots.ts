8480cddb6759b5af70a3e7c231e0795712f90217
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-12 20:23:21 -0300

    Add myChatbots context

---
+++ src/contexts/myChatbots.ts
@@ -0,0 +1,25 @@
+import { createContext } from 'react';
+
+export type MyChatbotsType = {
+  shortName: string;
+  name: string;
+  description: string;
+  image: string;
+  template: string;
+  created: string;
+  updated: string;
+  plan: string;
+  culture: string;
+  analytics: {
+    user: {
+      total: number;
+      actived: number;
+    };
+    message: {
+      received: number;
+      sent: number;
+    };
+  };
+}[]
+
+export const MyChatbotsContext = createContext<MyChatbotsType>([]);
--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -2,22 +2,28 @@ import React, { FC } from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
+import { MyChatbotsContext } from 'contexts/myChatbots';
+
+import myChatbots from 'data/myChatbots';
+
 import Header from 'core/Header';
 
 import MyChatbots from 'pages/MyChatbots';
 
 const App: FC = () => (
-  <main>
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/my-chatbots">
-          <MyChatbots />
-        </Route>
-        <Redirect to="/my-chatbots" />
-      </Switch>
-    </Router>
-  </main>
+  <MyChatbotsContext.Provider value={myChatbots}>
+    <main>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/my-chatbots">
+            <MyChatbots />
+          </Route>
+          <Redirect to="/my-chatbots" />
+        </Switch>
+      </Router>
+    </main>
+  </MyChatbotsContext.Provider>
 );
 
 export default App;
