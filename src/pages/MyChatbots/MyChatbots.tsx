696ae20071207d27fbf8f4d6bea1082a9919ffdb
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-12 19:38:56 -0300

    Add items layout

--- src/pages/MyChatbots/MyChatbots.tsx
+++ src/pages/MyChatbots/MyChatbots.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react';
 
 import organizeBlocks from 'assets/images/organize-blocks.png';
 import organizeList from 'assets/images/organize-list.png';
+import star from 'assets/images/star.png';
+import favorite from 'assets/images/favorite.png';
+
 import styles from './MyChatbots.module.scss';
 
 const MyChatbots: FC = () => (
@@ -20,6 +23,29 @@ const MyChatbots: FC = () => (
         </button>
       </div>
     </div>
+    <div className={styles.favorites}>
+      <h3 className={styles.title}>Favorites</h3>
+      <div className={styles.items}>
+        <div className={styles.item}>
+          <button className={styles.star}>
+            <img src={star} alt="" />
+          </button>
+          <div className={styles.image} />
+          <p className={styles.name}>Bot name</p>
+          <p className={styles.template}>Builder</p>
+        </div>
+      </div>
+    </div>
+    <div className={styles.items}>
+        <div className={styles.item}>
+          <button className={styles.star}>
+            <img src={favorite} alt="" />
+          </button>
+          <div className={styles.image} />
+          <p className={styles.name}>Bot name</p>
+          <p className={styles.template}>Builder</p>
+        </div>
+      </div>
   </section>
 );
 
