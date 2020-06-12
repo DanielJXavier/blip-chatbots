b685651d646b5a9e093d43aeff2ec7c255fd012f
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-12 19:38:11 -0300

    Add menu

--- src/pages/MyChatbots/MyChatbots.tsx
+++ src/pages/MyChatbots/MyChatbots.tsx
@@ -1,9 +1,26 @@
 import React, { FC } from 'react';
 
+import organizeBlocks from 'assets/images/organize-blocks.png';
+import organizeList from 'assets/images/organize-list.png';
 import styles from './MyChatbots.module.scss';
 
 const MyChatbots: FC = () => (
-  <section className={styles.myChatbots}></section>
+  <section className={styles.myChatbots}>
+    <div className={styles.menu}>
+      <h2 className={styles.title}>My chatbots</h2>
+      <div className={styles.actions}>
+        <input className={styles.search} type="text" placeholder="Search" />
+        <button className={styles.orderBy}>Order by name</button>
+        <button className={styles.orderBy}>Order by creation</button>
+        <button className={styles.showMode}>
+          <img src={organizeBlocks} alt="Show blocks" />
+        </button>
+        <button className={styles.showMode}>
+          <img src={organizeList} alt="Show list" />
+        </button>
+      </div>
+    </div>
+  </section>
 );
 
 export default MyChatbots;
