fc9851a3e83105f1492a753eefddaed7fef123e4
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-12 21:39:58 -0300

    Add MyChatbot types

--- src/contexts/myChatbots.ts
+++ src/contexts/myChatbots.ts
@@ -1,25 +1,5 @@
 import { createContext } from 'react';
 
-export type MyChatbotsType = {
-  shortName: string;
-  name: string;
-  description: string;
-  image: string;
-  template: string;
-  created: string;
-  updated: string;
-  plan: string;
-  culture: string;
-  analytics: {
-    user: {
-      total: number;
-      actived: number;
-    };
-    message: {
-      received: number;
-      sent: number;
-    };
-  };
-}[]
+import { MyChatbotsType } from 'data/myChatbots';
 
 export const MyChatbotsContext = createContext<MyChatbotsType>([]);
--- src/data/myChatbots.ts
+++ src/data/myChatbots.ts
@@ -1,3 +1,27 @@
+export type MyChatbotType = {
+  shortName: string;
+  name: string;
+  description: string;
+  image: string;
+  template: string;
+  created: string;
+  updated: string;
+  plan: string;
+  culture: string;
+  analytics: {
+    user: {
+      total: number;
+      actived: number;
+    };
+    message: {
+      received: number;
+      sent: number;
+    };
+  };
+};
+
+export type MyChatbotsType = MyChatbotType[];
+
 export default [
   {
     shortName: 'eleven',
