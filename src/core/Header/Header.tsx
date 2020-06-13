acb77d3a1e5b69dce04682920f8bd970c2c17f72
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-13 16:00:11 -0300

    Add link to MyChatbots in Header

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import logo from 'assets/images/logo.png';
 
 import styles from './Header.module.scss';
 
 const Header: FC = () => (
   <header className={styles.header}>
-    <img src={logo} alt="BLiP" />
+    <Link className={styles.link} to="/my-chatbots">
+      <img src={logo} alt="BLiP" />
+    </Link>
   </header>
 );
 
