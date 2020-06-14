56a7cebf135ad41e9b9aba435ec4521b97709ff8
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-14 13:49:47 -0300

    Add Header tests

---
+++ src/core/Header/Header.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Header from './Header';
+
+describe('Header component', () => {
+  test('Should render the component', () => {
+    const { container } = render(<Header />);
+
+    expect(container).toBeDefined();
+  });
+});
