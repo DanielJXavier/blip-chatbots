f9d72c288cc31e3aa079926c56ea94f03534b896
Daniel Xavier <dxdanielxavier@gmail.com>
2020-06-14 13:48:28 -0300

    Add ChatbotDetails tests

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -5,5 +5,10 @@ import '@testing-library/jest-dom/extend-expect';
 jest.mock('react-router-dom', () => {
   const Link: FC = ({ children }) => <a href="#link">{children}</a>;
 
-  return { Link };
+  const useParams = () => ({ shortName: 'billy_hargrove' });
+
+  return { 
+    Link, 
+    useParams 
+  };
 });
